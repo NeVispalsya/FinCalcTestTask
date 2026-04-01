47ee9a7baabba4414ca769e958a7ae784707f810
Nikita Stakovskiy <nstakovskiy@gmail.com>
2026-04-02 00:37:07 +0300

    Refactoring

--- src/main/java/com/FinCalc/Expression/ExpressionParser.java
+++ src/main/java/com/FinCalc/Expression/ExpressionParser.java
@@ -5,27 +5,30 @@ import com.FinCalc.Tokenization.Token;
 import com.FinCalc.Tokenization.TokenType;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExpressionParser {
     public Expression parse(List<Token> tokens){
-
-       for(Token token : tokens){
-           if(token.getType()==TokenType.PLUS||token.getType()==TokenType.MINUS){
-               parse(tokens);
-           }
-           if(token.getType()==TokenType.NUMBER){
-               String s = token.getValue().replaceAll("[^0-9.]", "");
-               if(token.getValue().charAt(0)=='$'){
-                   BigDecimal bd = new BigDecimal(s);
-                   MoneyExpression m = new MoneyExpression(new Money(bd, Currency.$));
-               }else if(token.getValue().toLowerCase().endsWith("eur")){
-                   BigDecimal bd = new BigDecimal(s);
-                   MoneyExpression m = new MoneyExpression(new Money(bd, Currency.eur));
-               }
-           }
-
-       }
-       return null;
+        if(!tokens.isEmpty()){
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).getType() == TokenType.PLUS || tokens.get(i).getType() == TokenType.MINUS) {
+                    List<Token> left = tokens.subList(0, i);
+                    List<Token> right = tokens.subList(i + 1, tokens.size());
+                    Expression leftExpr = parse(left);
+                    Expression rightExpr = parse(right);
+                    return new OperationExpression(leftExpr,rightExpr, tokens.get(i).getType());
+                }
+            }
+            String s = tokens.getFirst().getValue().replaceAll("[^0-9.]", "");
+            if(tokens.getFirst().getValue().charAt(0)=='$'){
+                BigDecimal bd = new BigDecimal(s);
+                return new MoneyExpression(new Money(bd, Currency.$));
+            }else if(tokens.getFirst().getValue().toLowerCase().endsWith("eur")){
+                BigDecimal bd = new BigDecimal(s);
+                return new MoneyExpression(new Money(bd, Currency.eur));
+            }
+        }
+        throw new RuntimeException("Invalid expression");
     }
 }
--- src/main/java/com/FinCalc/Main.java
+++ src/main/java/com/FinCalc/Main.java
@@ -1,5 +1,8 @@
 package com.FinCalc;
 
+import com.FinCalc.Expression.Expression;
+import com.FinCalc.Expression.ExpressionParser;
+import com.FinCalc.Money.Money;
 import com.FinCalc.Tokenization.Token;
 import com.FinCalc.Tokenization.Tokenizer;
 
@@ -7,11 +10,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "toDollar($10 + 5eur)";
+//        String input = "toDollar($10 + 5eur)";
+//        Tokenizer tokenizer = new Tokenizer();
+//        List<Token> tokens = tokenizer.tokenize(input);
+//        for (Token t : tokens) {
+//            System.out.println(t.getType() + " : " + t.getValue());
+//        }
+
+        String input = "$10 + 5eur";
+
         Tokenizer tokenizer = new Tokenizer();
         List<Token> tokens = tokenizer.tokenize(input);
-        for (Token t : tokens) {
-            System.out.println(t.getType() + " : " + t.getValue());
-        }
+
+        ExpressionParser parser = new ExpressionParser();
+        Expression expression = parser.parse(tokens);
+
+        Money result = expression.evaluate();
+
+        System.out.println(result.getAmount() + " " + result.getCurrency());
+
     }
 }
