85c1a13eb67b4bee65239a828d49a97f746a89d5
Nikita Stakovskiy <nstakovskiy@gmail.com>
2026-04-01 16:16:46 +0300

    add Tokenizer

--- src/main/java/com/FinCalc/Main.java
+++ src/main/java/com/FinCalc/Main.java
@@ -1,6 +1,14 @@
 package com.FinCalc;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String input = "toDollar($10 + 5eur)";
+        Tokenizer tokenizer = new Tokenizer();
+        List<Token> tokens = tokenizer.tokenize(input);
+        for (Token t : tokens) {
+            System.out.println(t.getType() + " : " + t.getValue());
+        }
     }
 }
--- src/main/java/com/FinCalc/Tokenizer.java
+++ src/main/java/com/FinCalc/Tokenizer.java
@@ -4,23 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tokenizer {
-    public List<Token> tokenize(String input){
-        StringBuilder sb = new StringBuilder();
-        List<Token> t = new ArrayList<>();
-        for(int i = 0; i < input.length(); i++){
-            if(Character.isLetter(input.charAt(i))){
-                sb.append(input.charAt(i));
-            } else if (Character.isDigit(input.charAt(i))) {
-                sb.append(input.charAt(i));
-            } else if (input.charAt(i)=='+') {
-                t.add(new Token("+",TokenType.PLUS));
-            } else if (input.charAt(i)=='-') {
-                t.add(new Token("-",TokenType.MINUS));
-            } else if (input.charAt(i)=='(') {
-                t.add(new Token("(",TokenType.LPAREN));
-            } else if (input.charAt(i)==')') {
-                t.add(new Token(")",TokenType.RPAREN));
+
+    public void addTokenFromBuilder(StringBuilder sb,List<Token> tokens){
+        if (!sb.isEmpty()) {
+            if(Character.isLetter(sb.charAt(0))){
+                tokens.add(new Token(sb.toString(),TokenType.FUNCTION));
+                sb.setLength(0);
+            }else{
+                tokens.add(new Token(sb.toString(),TokenType.NUMBER));
+                sb.setLength(0);
             }
         }
     }
+    public List<Token> tokenize(String input) {
+        List<Token> tokens = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isLetter(c) || Character.isDigit(c) || c == '$' || c == '.') {
+                sb.append(c);
+                continue;
+            }
+            addTokenFromBuilder(sb,tokens);
+            if (c == '+') {
+                tokens.add(new Token("+", TokenType.PLUS));
+            } else if (c == '-') {
+                tokens.add(new Token("-", TokenType.MINUS));
+            } else if (c == '(') {
+                tokens.add(new Token("(", TokenType.LPAREN));
+            } else if (c == ')') {
+                tokens.add(new Token(")", TokenType.RPAREN));
+            } else if (c == ' ') {
+                continue;
+            }
+        }
+        addTokenFromBuilder(sb,tokens);
+        return tokens;
+    }
+
 }
